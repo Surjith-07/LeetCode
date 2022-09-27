48b84753da709b954a5b421ac83ce8339db15914
A SURJITHRAJA <106502056+Surjith-07@users.noreply.github.com>
2022-09-27 15:28:43 +0530

    Time: 4 ms (49.88%), Space: 41.9 MB (69.01%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -4,14 +4,6 @@ class Solution {
         for(int i=0;i<arr.length;++i){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
-        List<Integer> li=new ArrayList<>();
-        for(Integer i:map.values()) li.add(i);
-        Collections.sort(li);
-        for(int i=0;i<li.size();i++){
-            for(int j=i+1;j<li.size();++j){
-                if(li.get(i).equals(li.get(j))) return false;
-            }
-        }
-        return true;
+        return map.size()==new LinkedHashSet<>(map.values()).size();
     }
 }
