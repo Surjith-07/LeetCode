74a5a9fa1d51118284f05f94438408d127a5e320
A SURJITHRAJA <106502056+Surjith-07@users.noreply.github.com>
2022-11-01 18:49:25 +0530

    Time: 8 ms (30.60%), Space: 43.4 MB (10.12%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
@@ -4,12 +4,12 @@ class Solution {
         String ans[]= s.split(" ");
         for(int i=0;i<ans.length;++i){
             if(getru(ans[i])){
-            if(max<Integer.parseInt(ans[i])){
-                max=Integer.parseInt(ans[i]);
+                int k=Integer.parseInt(ans[i]);
+            if(max<k){
+                max=k;
             }else return false;
             }
         }
-        // System.out.print(Arrays.toString(Arrays.copyOfRange(arr,0,p)));
         return true;
     }
     static boolean getru(String k){
