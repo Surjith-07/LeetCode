c425df54705a514edc95ff60334e2e8868690de4
A SURJITHRAJA <106502056+Surjith-07@users.noreply.github.com>
2022-11-02 17:33:36 +0530

    Time: 242 ms (11.09%), Space: 43.1 MB (86.75%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
@@ -1,9 +1,8 @@
 class Solution {
     public String[] findRestaurant(String[] l1, String[] l2) {
-        int min=Integer.MAX_VALUE;
-        int arr[]=new int[l1.length+l2.length],p=0;
-        for(int i=0;i<l1.length;++i){
-            for(int j=0;j<l2.length;++j){
+        int min=Integer.MAX_VALUE,len1=l1.length,len2=l2.length;
+        for(int i=0;i<len1;++i){
+            for(int j=0;j<len2;++j){
                 if(l1[i].equals(l2[j])){
                     if(min>(i+j)){
                         min=(i+j);
@@ -11,10 +10,10 @@ class Solution {
                 }
             }
         }
-         String ans[]=new String[arr.length];
-        int q=0;
-        for(int i=0;i<l1.length;++i){
-            for(int j=0;j<l2.length;++j){
+         String ans[]=new String[len1+len2];
+            int q=0;
+        for(int i=0;i<len1;++i){
+            for(int j=0;j<len2;++j){
                 if(l1[i].equals(l2[j])){
                     if(min==i+j){
                         ans[q++]=l1[i];
