701840788748b615520079d6c842063d215183f1
A SURJITHRAJA <106502056+Surjith-07@users.noreply.github.com>
2022-11-02 11:03:02 +0530

    Time: 20 ms (5.25%), Space: 44.8 MB (5.21%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,33 +1,33 @@
 import java.math.BigInteger;
 class Solution {
     public int calPoints(String[] op) {
-        Stack<BigInteger> boss=new Stack<>(); //["5","-2","4","C","D","9","+","+"]
+        Stack<Integer> boss=new Stack<>(); //["5","-2","4","C","D","9","+","+"]
         for(String s:op){
             if(chck(s)){
-                BigInteger n=new BigInteger(s);
+                Integer n=Integer.parseInt(s);
                 boss.push(n);
             }
             else if(!boss.isEmpty()){
             if(s.equals("C")){
                 boss.pop();
             }if(s.equals("D")){
-                BigInteger l=boss.pop();
+                Integer l=boss.pop();
                 boss.push(l);
-                boss.push(l.multiply(new BigInteger(2+"")));
+                boss.push(l*2);
             }if(s.equals("+")){
-                BigInteger v1=boss.pop();
-                BigInteger v2=boss.pop();
-                boss.push(v2); boss.push(v1); boss.push(v1.add(v2));
+                Integer v1=boss.pop();
+                Integer v2=boss.pop();
+                boss.push(v2); boss.push(v1); boss.push(v1+v2);
                 }
         }   
     }
-        BigInteger sum=new BigInteger(0+"");
-        while(!boss.isEmpty()){ sum=sum.add(boss.pop());}
-        return sum.intValue();
+        int sum=0;
+        while(!boss.isEmpty()){ sum+=boss.pop();}
+        return sum;
     }
     public static boolean chck(String s){
         try{
-            BigInteger b=new BigInteger(s);
+            int b=Integer.parseInt(s);
             return true;
         } catch(Exception e){return false;}
     }
