e01c9948b498f1409d2c31f830e80cec7dcd1696
A SURJITHRAJA <106502056+Surjith-07@users.noreply.github.com>
2022-11-02 10:58:51 +0530

    Time: 11 ms (8.81%), Space: 42.1 MB (65.70%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,33 @@
+import java.math.BigInteger;
+class Solution {
+    public int calPoints(String[] op) {
+        Stack<BigInteger> boss=new Stack<>(); //["5","-2","4","C","D","9","+","+"]
+        for(String s:op){
+            if(chck(s)){
+                BigInteger n=new BigInteger(s);
+                boss.push(n);
+            }
+
+            if(s.equals("C")){
+                boss.pop();
+            }if(s.equals("D")){
+                BigInteger l=boss.pop();
+                boss.push(l);
+                boss.push(l.multiply(new BigInteger(String.valueOf(2))));
+            }if(s.equals("+")){
+                BigInteger v1=boss.pop();
+                BigInteger v2=boss.pop();
+                boss.push(v2); boss.push(v1); boss.push(v1.add(v2));
+                }
+        }
+        BigInteger sum=new BigInteger(String.valueOf(0));
+        while(!boss.isEmpty()){ sum=sum.add(boss.pop());}
+        return sum.intValue();
+    }
+    public static boolean chck(String s){
+        try{
+            BigInteger b=new BigInteger(s);
+            return true;
+        } catch(Exception e){return false;}
+    }
+}
