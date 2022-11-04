68bc3f3cecd7b41a07ce86379b83a85d04cfc7c7
A SURJITHRAJA <106502056+Surjith-07@users.noreply.github.com>
2022-11-04 10:39:29 +0530

    Time: 2 ms (73.37%), Space: 54.3 MB (40.30%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] findBall(int[][] grid) {
         int r=grid.length,c=grid[0].length;
-        int ans[]=new int[c];
+        int arr[]=new int[c];
+        int ans=-1;
         for(int i=0;i<c;++i){
-            ans[i]=getans(0,i,r,c,grid);
+        arr[i]=getans(0,i,r,c,grid);
         }
-        return ans;
+        return arr;
     }
     int getans(int r,int c,int limR,int limC,int mat[][]){
-        //base mat[i][j] near mat[i][j+1] mat[i-1][j]
         if(c<0 || c>=limC) return -1;
         if(r==limR) return c;
         if(mat[r][c]==1 && c+1 < limC && mat[r][c+1]==1){
